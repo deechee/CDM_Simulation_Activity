367ff0ad7543438a6c0b8983329d852fd78dc788
Huike Cheng <huikecheng@outlook.com>
2022-12-01 11:09:38 +0000

    edit code for name

--- Simulation.py
+++ Simulation.py
@@ -11,20 +11,31 @@ fake = Faker()
 # set seed
 random.seed(0) # not sure if this is how to do it
 
-# name (First and Last)
-name = []
-for i in range(500):
-  name.append(fake.name())
-print(name)
-
-# sample_id (any reference of your choice)
-
-# age
+# country
 
 # gender
 gender = np.random.randint(2, size=500)
 gender = np.where(gender == 1, "Male", "Female")
 gender
+
+# name (First and Last)
+def gen_name(x):
+    if x == "Female":
+        return fake.name_female()
+    else: 
+        return fake.name_male()
+
+name = gender.apply(lambda x: gen_name(gender[x]))
+
+#name = []
+#for i in range(500):
+#  name.append(fake.name())
+#print(name)
+
+# sample_id (any reference of your choice)
+
+
+# age
 
 # bmi
 
@@ -41,3 +52,6 @@ gender
 # 5 SNP values (0,1,2)
 
 # case_control status defined as a function of some of your other variables
+# logit_p = 0.2*bmi + 0.6*I(SNP1 == 1) + 1.2*I(SNP1 == 2)
+# p = 1/(1+exp(-(logit_p)))
+# status ~ binomial(1, p)
